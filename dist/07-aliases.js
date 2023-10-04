3e4bcc3f72654307a46b14a1c638eaf69316d2ba
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-04 23:36:39 +0300

    added example to use aliases

---
+++ dist/07-aliases.js
@@ -0,0 +1,18 @@
+"use strict";
+// Алиас это всё то, что мы обозначаем словом type.
+let value;
+value = 1; // OK
+value = 2; // OK
+let answer;
+answer = 'yes'; // OK
+answer = 'no'; // OK
+const userType = {
+    name: 'Alexander',
+    age: 36,
+    skills: ['active', 'tolerance'],
+};
+function fetchWithAuth(url, method) {
+    return 1;
+}
+fetchWithAuth('url', 'GET');
+//# sourceMappingURL=07-aliases.js.map
---
+++ src/07-aliases.ts
@@ -0,0 +1,63 @@
+// Алиас это всё то, что мы обозначаем словом type.
+
+// Такую запись как:
+
+// type Status = 'idle' | 'loading' | 'finished' | 'rejected';
+
+// надо читать как "эй ts, создай мне алиас типа под именем Status и сохрани в него тип юниона, который состоит из таких-то конкретных литералов".
+
+// Под алиасом может храниться что угодно - сигнатура функции, набор свойств объекта, массив или кортеж...
+
+// А юнион в свою очередь - это конкретный тип, который можно как сохранить под конкретным именем(алиасом), так и использовать напрямую в качестве описания типа параметров функции или свойств объекта.
+
+type OneOrTwo = 1 | 2;
+let value: OneOrTwo;
+value = 1; // OK
+value = 2; // OK
+// value = 3; // Error: Type '3' is not assignable to type 'OneOrTwo'.
+
+type YesOrNo = 'yes' | 'no';
+let answer: YesOrNo;
+answer = 'yes'; // OK
+answer = 'no'; // OK
+// answer = 'maybe'; // Error: Type '"maybe"' is not assignable to type 'YesOrNo'.
+
+
+type MyBoolean = false | true;
+type mixedType = string | number | boolean;
+
+type Pair = [string, string];
+type Pairs = Pair[];
+
+
+type DiffCar = {
+    wheels: number;
+    brand: string;
+    type: string;
+    isNew?: boolean;
+    name?: string;
+
+    [key: string]: unknown;
+}
+
+type UserType = {
+    name: string,
+    age: number,
+    skills: string[],
+}
+
+const userType: UserType = {
+    name: 'Alexander',
+    age: 36,
+    skills: ['active', 'tolerance'],
+}
+
+type UrlStr = string;
+type HttpMethod = 'POST' | 'GET'
+
+function fetchWithAuth(url: UrlStr, method: HttpMethod): 1 | -1 {
+    return 1;
+}
+
+fetchWithAuth('url', 'GET');
+
