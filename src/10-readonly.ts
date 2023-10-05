1654e3e846705614e9849c3575c5133672aa5280
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-05 21:44:56 +0300

    added examples to use readonly

---
+++ src/10-readonly.ts
@@ -0,0 +1,15 @@
+// const skill: [number, string] = [1, 'Dev'];
+// skill[0] = 2 // ok - change
+
+// readonly  - say that const skill = [1, 'Dev'];  is tuple and can`t be changed
+
+const skill: readonly [number, string] = [1, 'Dev'];
+// skill[0] = 2;  // error
+
+const skillX: readonly string[] = ['Dev', 'QA']
+// skillX[0] = ''; // error
+
+const skillXx: ReadonlyArray<string> = ['Dev', 'QA']
+// skillXX[0] = ''; // error
+
+// skill.push('') // error
