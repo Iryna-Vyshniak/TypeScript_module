833b9a6805ad0ec9a38288ea55d76f39ed2919b6
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-08 23:09:13 +0300

    complete examples demonstrating the usage optional

---
+++ dist/02-intermediate/06-optional.js
@@ -0,0 +1,24 @@
+"use strict";
+const user = {
+    login: 'q@q.ua',
+    password: 'qwerty',
+};
+function multiply(a, b = 5, c) {
+    if (!c) {
+        return a * b;
+    }
+    return a * b * c;
+}
+multiply(5);
+function testPass(user) {
+    var _a;
+    // const t = user.password ?  user.password.type : undefined;
+    const t = (_a = user.password) === null || _a === void 0 ? void 0 : _a.type;
+}
+function testPassX(user) {
+    const t = user.password.type; // sure that the password will never be undefined, always have type
+}
+function test(params) {
+    const t = params && multiply(5); // if params is undefined or null -> do function multiply(5)
+}
+//# sourceMappingURL=06-optional.js.map
---
+++ src/02-intermediate/06-optional.ts
@@ -0,0 +1,42 @@
+interface User {
+    login: string,
+    password?: string
+}
+
+const user: User = {
+    login: 'q@q.ua',
+    password: 'qwerty',
+}
+
+function multiply(a: number, b: number = 5, c?: number): number {
+    if (!c) {
+        return a * b;
+    }
+    return a * b * c
+}
+
+multiply(5)
+
+
+interface UserPro {
+    login: string,
+    password?: {
+        type: 'primary' | 'secondary'
+    }
+}
+
+function testPass(user: UserPro) {
+    // const t = user.password ?  user.password.type : undefined;
+    const t = user.password?.type;
+}
+
+function testPassX(user: UserPro) {
+    const t = user.password!.type; // sure that the password will never be undefined, always have type
+}
+
+
+function test(params?: string) {
+    const t = params && multiply(5) // if params is undefined or null -> do function multiply(5)
+}
+
+
