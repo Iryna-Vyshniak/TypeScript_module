2b246939a7ff1da1dc6fd6a6f1afaebb1fadde9e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-14 20:07:55 +0300

    complete examples demonstrating the generic syntax

--- dist/03-generic/01-why-generic.js
+++ dist/03-generic/01-why-generic.js
@@ -1,8 +1,23 @@
-"use strict";
+/* Узагальнені типи (Generics) - це один із потужних інструментів TypeScript, що допомагають створювати код, який можна використовувати повторно, зберігаючи водночас строгу типізацію. Дозволяють визначити "узагальнений" тип, який потім може бути спеціалізований для роботи з різними іншими типами. Замість того, щоб визначати окремі функції для кожного можливого типу даних, ви можете визначити одну функцію, яка працює з "будь-яким" типом даних.*/
 function head(value) {
     return value[0];
 }
-head('string');
-head([1, 2, 4, 34]);
-head([true, false, true, false]);
+head('certain');
+const obj1 = {
+    title: 'Model',
+    value: 12
+};
+// obj1.value = true // ERROR
+obj1.value = 5; // OK
+export const obj2 = {
+    title: 'Model',
+    value: [12, 4]
+};
+// obj1.value = true // ERROR
+// obj2.value = 5 // ERROR
+obj2.value.push(1); // OK
+// ----------------------------------------------------------------
+let arr = [];
+// arr = ['str', 10, true]; // Error
+arr = ['str', 10]; // OK
 //# sourceMappingURL=01-why-generic.js.map
--- src/03-generic/01-why-generic.ts
+++ src/03-generic/01-why-generic.ts
@@ -1,19 +1,60 @@
+/* Узагальнені типи (Generics) - це один із потужних інструментів TypeScript, що допомагають створювати код, який можна використовувати повторно, зберігаючи водночас строгу типізацію. Дозволяють визначити "узагальнений" тип, який потім може бути спеціалізований для роботи з різними іншими типами. Замість того, щоб визначати окремі функції для кожного можливого типу даних, ви можете визначити одну функцію, яка працює з "будь-яким" типом даних.*/
+
+// function head(value: string): string;
+// function head(value: number[]): number;
+// function head(value: string[]): string;
+// function head(value: boolean[]): boolean;
+// function head(value: Date[]): Date;
+
+// function head(value: any): any {
+//     return value[0];
+// }
+
+// head('string');
+// head([1, 2, 4, 34]);
+// head([true, false, true, false]);
+
 function head(value: string): string;
-function head(value: number[]): number;
-function head(value: string[]): string;
-function head(value: boolean[]): boolean;
-function head(value: Date[]): Date;
+function head(value: readonly []): undefined;
+function head<T>(value: readonly T[]): T;
 
 function head(value: any): any {
     return value[0];
 }
 
-head('string');
-head([1, 2, 4, 34]);
-head([true, false, true, false]);
+head('certain');
 
+// interface IModelData<T> {
+//     title: string
+//     value: string //  or number[]  or boolean
+// }
 
-interface IModelData {
+interface IModelData<T> {
     title: string
-    value: string // number[] boolean
+    value: T
 }
+
+const obj1: IModelData<number> = {
+    title: 'Model',
+    value: 12
+}
+
+// obj1.value = true // ERROR
+obj1.value = 5 // OK
+
+export const obj2: IModelData<Array<number>> = {
+    title: 'Model',
+    value: [12, 4]
+}
+
+// obj1.value = true // ERROR
+// obj2.value = 5 // ERROR
+obj2.value.push(1) // OK
+
+
+// ----------------------------------------------------------------
+
+let arr: Array<string | number> = [];
+
+// arr = ['str', 10, true]; // Error
+arr = ['str', 10]; // OK
